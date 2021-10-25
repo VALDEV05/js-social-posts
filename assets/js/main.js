57d83235c0daaca4a82a3ed8799e255b2f84b328
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-25 17:15:10 +0200

    Insert post with JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,24 +29,56 @@ const posts = [{
     profilePicture: 'https://picsum.photos/50/50',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit consectetur cumque alias consequuntur dolorum aspernatur!Odit consectetur cumque alias consequuntur',
     image: 'https://picsum.photos/450/500'
+}, {
+    name: 'Phil Iamocela',
+    profilePicture: 'https://picsum.photos/50/50',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit consectetur cumque alias consequuntur dolorum aspernatur!Odit consectetur cumque alias consequuntur',
+    image: 'https://picsum.photos/450/500'
 }]
 
+
+
+
+//stampa a schermo
+for (let index = 0; index < posts.length; index++) {
+    console.log(posts[index].name);
+    console.log(posts[index].profilePicture);
+    console.log(posts[index].description);
+    console.log(posts[index].image);
+    post = `<div class="post bg-light my-5">
+            <div class="userData user">
+                <div class="image_user"></div>
+                <div class="name_user"></div>
+            </div>
+            <div class="userData description">
+            </div>
+            <div class="image_post">
+                <img src="..." alt="">
+            </div>
+            <div class="userData like bg-light">
+                <button class="btnLike d-flex justify-content-center align-items-center text-dark"><i class="fas fa-thumbs-up"></i> &nbsp;MI PIACE</button>
+                <div class="counterLike d-flex justify-content-center align-items-center"></div>
+            </div>
+        </div>`
+    placeToPrint.innerHTML += post
+
+
+}
 const nameDataUser = document.querySelector('.name_user');
 const profilePicDataUser = document.querySelector('.image_user');
 const descriptionDataUser = document.querySelector('.userData.description');
 const imageDataUser = document.querySelector('.image_post')
 const btnLike = document.querySelector('.btnLike');
 const placeLike = document.querySelector('.counterLike')
+
 placeLike.innerHTML = `<p>Piace a <span class="fw-bold">0</span> persone</p>`
-
-
-let like = 0
-btnLike.addEventListener('click', function() {
-    like++;
-    placeLike.innerHTML = `<p>Piace a <span class="fw-bold">${like}</span> persone</p>`
-})
 
 nameDataUser.innerHTML = `<p>${posts[0].name}</p>`
 profilePicDataUser.innerHTML = `<img src=${posts[0].profilePicture}>`
 descriptionDataUser.innerHTML = `<p>${posts[0].description}</p>`
 imageDataUser.innerHTML = `<img src=${posts[0].image}>`
+let like = 0
+btnLike.addEventListener('click', function() {
+    like++;
+    placeLike.innerHTML = `<p>Piace a <span class="fw-bold">${like}</span> persone</p>`
+})
